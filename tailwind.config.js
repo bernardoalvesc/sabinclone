f647cbbdbfa2cd9c6f5c4f3095057eb3736acfca
bernardoalvesc <bernardocardoso2406@gmail.com>
2025-08-05 14:21:36 -0300

    chore(assets): add exported images from Figma

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1140px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
